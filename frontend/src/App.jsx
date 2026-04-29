93d7ad18ff3ebcc05920076f3f660995a44ce5eb
MatiasCortes2211 <mcortes@alu.frlp.utn.edu.ar>
2026-04-28 22:00:16 -0300

    feat(servicios): agrega componente de servicios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/layout/Navbar'
 import Hero from './components/sections/Hero'
 import Nosotros from './components/sections/Nosotros'
+import Servicios from './components/sections/Servicios'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Navbar />
         <Hero />
         <Nosotros />
+        <Servicios />
     </div>
   )
 }
---
+++ frontend/src/components/sections/Servicios.jsx
@@ -0,0 +1,23 @@
+function Servicios() {
+  return (
+    <section>
+      <h2>Lo que ofrecemos</h2>
+      <div>
+        <div>
+          <h3>Desarrollo Full Stack</h3>
+          <p>Lorem ipsum</p>
+        </div>
+        <div>
+          <h3>E-commerce</h3>
+          <p>Lorem ipsum</p>
+        </div>
+        <div>
+          <h3>Desarrollo Mobile</h3>
+          <p>Lorem ipsum</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Servicios
